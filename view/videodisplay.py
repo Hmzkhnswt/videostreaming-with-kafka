5e195ff8be3bd2d96f98ed0e2c543cea75bc1f33
Hmzkhnswt <hamzakhanswati117191@gmail.com>
2024-06-16 00:01:07 +0500

    Moved from vidgear to yt_dlp

--- view/videodisplay.py
+++ view/videodisplay.py
@@ -2,8 +2,9 @@ import cv2
 import os
 from vidgear.gears import CamGear
 from dotenv import load_dotenv
+from view.videostream import VideoStream
 
-load_dotenv(dotenv_path = '.env.dev')
+load_dotenv(dotenv_path = '../.env.dev')
 youtube_url = os.getenv('YOUTUBE_URL')
 
 class VideoDisplay:
@@ -12,18 +13,12 @@ class VideoDisplay:
         self.storing_dir = storing_dir
         os.makedirs(self.storing_dir, exist_ok=True)
         self.frame_counter = 0
-    
-    def show_frame(self, frame):
-        cv2.imshow("Frame", frame)
         
-        # Save the frame
-        frame_path = os.path.join(self.storing_dir, f"frame{self.frame_counter}.jpg")
-        cv2.imwrite(frame_path, frame)
-        self.frame_counter += 1
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            return False
-        return True
+    def SavingFrmaes(self):
+        Stream = VideoStream(youtube_url)
+        Frames = Stream.read_frame()
+        while Frames is not None:
+            self.frame_counter += 1
+            cv2.imwrite(f"{self.storing_dir}/frame_{self.frame_counter}.jpg", Frames)
 
-    def close(self):
-        cv2.destroyAllWindows()
+        
